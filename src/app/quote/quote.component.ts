19b95b9af6243d53a0dc9ca079a1bc2c97168e08
joan-w <wamuyujoan4@gmail.com>
2020-04-01 17:44:11 +0300

    Adding functionalities in quote component ts file

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,39 +1,39 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Quote } from '../quote';
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
-// @Component({
-//   selector: 'app-quote',
-//   templateUrl: './quote.component.html',
-//   styleUrls: ['./quote.component.css']
-// })
-// export class QuoteComponent implements OnInit {
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
 
-//   quotes:Quote[] = [
-//     new Quote(1,'"Work hard today for a better tomorrow"', "Eustus", new Date(2022,0,14)),
-//     new Quote(2,'"Steadfastly seeking excellence"', "Simon", new Date(2020,2,11)),
-//     new Quote(3,'"Never do anything to impress others, impress yourself first"', "Joan", new Date(2018,1,11)),
-//   ];
+  quotes:Quote[] = [
+    new Quote(1,'"Work hard today for a better tomorrow"', "Eustus", new Date(2022,0,14)),
+    new Quote(2,'"Steadfastly seeking excellence"', "Simon", new Date(2020,2,11)),
+    new Quote(3,'"Never do anything to impress others, impress yourself first"', "Joan", new Date(2018,1,11)),
+  ];
 
-//   deleteQuote(isDelete, index){
-//     if (isDelete) {
-//       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+  deleteQuote(isDelete, index){
+    if (isDelete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
 
-//       if (toDelete) {
-//         this.quotes.splice(index,1)
-//       }
-//     }
-//   }
+      if (toDelete) {
+        this.quotes.splice(index,1)
+      }
+    }
+  }
 
-//   publishNewQuote(quote){
-//     let quoteLength = this.quotes.length;
-//     quote.id = quoteLength+1;
-//     quote.completeDate = new Date(quote.completeDate)
-//     this.quotes.push(quote)
-//   }
+  publishNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
 
-//   constructor() { }
+  constructor() { }
 
-//   ngOnInit(): void {
-//   }
+  ngOnInit(): void {
+  }
 
-// }
+}
