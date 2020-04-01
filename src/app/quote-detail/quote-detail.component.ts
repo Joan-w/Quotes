50975a43a31f7cb3996ec96a4c27eaeec3732ac4
joan-w <wamuyujoan4@gmail.com>
2020-04-01 17:44:30 +0300

    Adding functionalities in quote-detail component ts file

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,22 +1,22 @@
-// import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-// import { Quote } from '../quote';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
-// @Component({
-//   selector: 'app-quote-detail',
-//   templateUrl: './quote-detail.component.html',
-//   styleUrls: ['./quote-detail.component.css']
-// })
-// export class QuoteDetailComponent implements OnInit {
+@Component({
+  selector: 'app-quote-detail',
+  templateUrl: './quote-detail.component.html',
+  styleUrls: ['./quote-detail.component.css']
+})
+export class QuoteDetailComponent implements OnInit {
 
-//   @Input() quote: Quote;
-//   @Output() isDelete = new EventEmitter<boolean>();
+  @Input() quote: Quote;
+  @Output() isDelete = new EventEmitter<boolean>();
 
-//   quoteDelete(erase:boolean){
-//     this.isDelete.emit(erase);
-//   }
-//   constructor() { }
+  quoteDelete(erase:boolean){
+    this.isDelete.emit(erase);
+  }
+  constructor() { }
 
-//   ngOnInit(): void {
-//   }
+  ngOnInit(): void {
+  }
 
-// }
+}
